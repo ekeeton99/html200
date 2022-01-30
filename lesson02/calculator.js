a2908f4d19507563a053c7668b0298bfdf8c0121
Emmett Keeton <emmaleek9@yahoo.com>
2022-01-29 19:16:59 -0800

    Completed lesson 2 challenge 1 assignment

--- lesson02/calculator.js
+++ lesson02/calculator.js
@@ -7,13 +7,13 @@ if (a == 1) {
   console.log('one');
 }
 if (a + 1 == 2) {
-  console.log(b)
+  console.log(b);
 }
 if (a * b >= c) {
-  console.log('true')
+  console.log('true');
 } else {
-  console.log('false')
+  console.log('false');
 }
 if (d * 5 > 10) {
-  console.log('more than 10')
+  console.log('more than 10');
 }
